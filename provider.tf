dddbb27b65db7b5383e575ce6dc51109a2155046
yusuf <yusufabdulganiyu@outlook.com>
2024-06-01 14:54:23 +0100

    modified provider and cidr

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 locals {
   region = "us-east-1"
   name   = "flask-eks-cluster"
-  vpc_cidr = "10.123.0.0/16"
+  vpc_cidr = "10.23.0.0/16"
   azs      = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
-  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
+  public_subnets  = ["10.23.1.0/24", "10.23.2.0/24"]
+  private_subnets = ["10.23.3.0/24", "10.23.4.0/24"]
   # intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
 }
 
