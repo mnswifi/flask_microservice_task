9048c776ec46f2fb326952933348d4b4053a1b2f
yusuf <yusufabdulganiyu@outlook.com>
2024-06-01 14:51:06 +0100

    edited backend config

--- Backend.tf
+++ Backend.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 backend "s3" {
-    bucket = "flask_eks_task-tfstate"
+    bucket = "flask_eks_task_tfstate"
     key = "state/terraform.tfstate"
     region = "us-east-1"
     encrypt = true
--- vpc.tf
+++ vpc.tf
@@ -13,6 +13,8 @@ module "vpc" {
   single_nat_gateway = true
   enable_dns_hostnames = true
 
+  map_public_ip_on_launch = true
+
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
   }
